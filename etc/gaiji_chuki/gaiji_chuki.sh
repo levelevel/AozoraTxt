ea9ee0c68f9329b35cf229fe5dfd63a5f091d063
Hideyuki Emura <level@mub.biglobe.ne.jp>
2020-08-15 11:28:09 +0900

    コード無し外字注記

---
+++ etc/gaiji_chuki/gaiji_chuki.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -u
+
+ROOT=`dirname $0`
+ORG=${ROOT}/gaiji_chuki_grep.txt
+F1=${ROOT}/gaiji_chuki_sort.txt
+F2=${ROOT}/gaiji_chuki_uniq.txt
+
+sed -e '1,/^◎/d' -e '/が検索されました/d' $ORG | grep -v '^■' | sed 's/^・.\+: ※［＃//' | sed 's/、.*$//' | sort > $F1
+uniq -c $F1 > $F2
