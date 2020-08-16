23cc7ffe93dab7136fc18e7d16c4b91094e58998
Hideyuki Emura <level@mub.biglobe.ne.jp>
2020-08-16 17:34:35 +0900

    コード無し外字注記：抽出方法を変更：全外注注記抽出ログからコードありを除外して作成

--- etc/gaiji_chuki/gaiji_chuki.sh
+++ etc/gaiji_chuki/gaiji_chuki.sh
@@ -3,13 +3,16 @@
 set -u
 
 ROOT=`dirname $0`
-ORG=${ROOT}/gaiji_chuki_grep.txt
+ORG=${ROOT}/all/gaiji_chuki_all_grep.txt
 F1=${ROOT}/gaiji_chuki_sort.txt
 F2=${ROOT}/gaiji_chuki_uniq.txt
 
-sed -e '1,/^◎/d' -e '/が検索されました/d' $ORG |
-    grep -v '^■' |
-    sed 's/^・.\+: ※［＃//' |
-    sed 's/、[^、]*$//' |
+sed -e '1,/^■/d' -e 's/^.*: ※［＃//' -e '/が検索されました/d' $ORG |
+    egrep -v '、U\+[0-9A-F]+、'         | #Unicode
+    egrep -v '第[34]水準[-0-9]+」?］$'  |
+    egrep -v '、[0-9]+-[0-9]+-[0-9]+］$'     | #1-13-21
+    sed -e 's/、[-0-9上中下段巻序一ニ三四五六七八九はしがきコマ右左（）目次脚注本文－」]\+］//' -e 's/[、］]$//'| #末尾のページ番号取る
+    egrep -v '、U\+[0-9A-F]+$'       | #ページ番号なし救済
+    egrep -v '、第[34]水準[-0-9]+$'  | #ページ番号付き救済
     sort > $F1
-uniq -c $F1 > $F2
+    uniq -c $F1 > $F2
