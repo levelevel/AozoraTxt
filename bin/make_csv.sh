e9255ce60283cce56be370d30e817ce919315401
levelevel <level@mub.biglobe.ne.jp>
2021-01-01 22:23:45 +0900

    make_csv.sh:コメント追加

--- bin/make_csv.sh
+++ bin/make_csv.sh
@@ -13,10 +13,10 @@ person_from=$BIN/../person_utf8
 
 echo "作家ID,作家名" > $NAME_LIST
 echo "ファイル名,作品名,作家名,訳者名" > $TITLE_LIST
-tmp=$TITLE_LIST.tmp
 old_person=0
 while read txt
 do
+    #作者名は図書カードの作家データから取得する。姓名が分かち書きされているので本文から取得するよりは有利。
     person=${txt#*person_utf8/}
     person=${person%%/*}
     if [ $old_person != $person ]; then
@@ -31,7 +31,23 @@ do
         #name=${name%%<*}
         echo $person,$name >> $NAME_LIST
     fi
-    mapfile -t array < <(sed -n 1,/^$/p "$txt")
+    #本文冒頭は以下の4パターンがある。
+    #1:タイトル
+    #2:作者
+    #
+    #1:タイトル
+    #2:サブタイトル
+    #3:作者
+    #
+    #1:タイトル
+    #2:作者
+    #3:翻訳者
+    #
+    #1:タイトル
+    #2:サブタイトル
+    #3:作者
+    #4:翻訳者
+    mapfile -t array < <(head -5 "$txt" | sed -n 1,/^$/p)
     #echo ${array[@]}
     title=${array[0]}
     trans=""
